37d02f80a73a1c0ece01243fd07aa52f6ec673c4
Praveen Hasaranga <info.praveenhasaranga@gmail.com>
2020-10-28 14:44:42 +0530

    Second Commit 02.45 AM 28/10/2020

--- demoapp/models.py
+++ demoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    category_choice = (
+        ('Category A', 'Category A'),
+        ('Category B', 'Category B'),
+        ('Category C', 'Category C'),
+    )
+
+    task_no = models.CharField(max_length=7, verbose_name='Task No')
+    sub_task_no = models.CharField(max_length=50, verbose_name='Sub Task No', null=True)
+    task_value = models.IntegerField(null=True, verbose_name='Task Value')
+    category = models.CharField(max_length=50, choices=category_choice)
+    stage = models.CharField(max_length=50, default='stage1')
+
+    def __str__(self):
+        return f"{self.sub_task_no}"
+
--- demoapp/views.py
+++ demoapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 
 def IndexPage(request):
-    return render(request, 'index.html')
+    stage1 = Task.objects.filter(stage='stage1')
+    stage2 = Task.objects.filter(stage='stage2')
+    stage3 = Task.objects.filter(stage='stage3')
+    stage4 = Task.objects.filter(stage='stage4')
+    stage5 = Task.objects.filter(stage='stage5')
+
+    context = {'stage1': stage1, 'stage2': stage2, 'stage3': stage3, 'stage4': stage4, 'stage5': stage5}
+    return render(request, 'index.html', context)
